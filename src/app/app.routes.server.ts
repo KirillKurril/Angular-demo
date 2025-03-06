7efa924a2170152cadc811598fa7523250ea851b
KirillKurril <kirillkurill1525@gmail.com>
2025-03-07 00:57:16 +0300

    init

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '', 
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about', 
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'catalog', 
+    renderMode: RenderMode.Server,
+  },  
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'catalog', component: CatalogComponent }
+  ];
